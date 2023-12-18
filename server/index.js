7bdf7675623039d3f2766d7a7191eed5937bcfa3
Brandon2X <bolivencia02@gmail.com>
2023-12-18 16:17:58 -0500

    trying to figure out why the website is not showing

--- server/index.js
+++ server/index.js
@@ -3,14 +3,23 @@ const path = require('path');
 
 const app = express();
 const PORT = 3000;
-const clientPath = path.resolve(__dirname, '../client/dist');
+const clientPath = path.resolve(__dirname, '../client');
 
 app.use(express.static(clientPath));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../client', 'index.html'));
 });
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
+
+
+
+
+
+
+
+// ...
+
