03d563d2cf9d40497c7c13b6c8062702a6e547f3
Brandon2X <bolivencia02@gmail.com>
2023-12-18 16:45:08 -0500

    moved the client side html and css file to thre server to try to get it to run

--- server/index.js
+++ server/index.js
@@ -5,15 +5,24 @@ const app = express();
 const PORT = 3000;
 const clientPath = path.resolve(__dirname, '../client');
 
-app.use(express.static(clientPath));
+app.use(express.static(clientPath, {
+  setHeaders: (res, filePath) => {
+    if (filePath.endsWith('.css')) {
+      res.setHeader('Content-Type', 'text/css');
+    }
+  }
+}));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
 });
 
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
+
+
+
 
 
 
